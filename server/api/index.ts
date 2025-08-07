715c2d6cbc8c3e2062b64a1273700501f9e8e940
ReviewBoosted <224332340+reviewboosted@users.noreply.github.com>
2025-08-06 21:32:39 -0400

    added user route

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,17 +1,20 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 dotenv.config();
+
+import useRoute from "./user/user";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/user", useRoute);
 
-app.get('/api', (req: Request, res: Response) => {
-  res.send('Hello from the application API!');
+app.get("/api/message", (req: Request, res: Response) => {
+    res.send("Hello from the application API!");
 });
 
 app.listen(3000, () => {
-  console.log(`Server is running on port 3000`);
+    console.log(`Server is running on port 3000`);
 });
